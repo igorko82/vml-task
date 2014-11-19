9d8a8e07da0834f9d5da5b2c74415d1f5c83b6cb
Игорь <igorko82@me.com>
2014-11-19 10:27:15 +0100

    init a repo

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    $('.menu-handler').click(function(){
+    	$('.main-menu').toggleClass('show');
+    });
+  });
+
+})(jQuery, window, document);
